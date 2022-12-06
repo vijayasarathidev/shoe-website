820c37081b1a530d0e7d0278e07aa1ae6867a69c
vijayasarathidev <emailvijayasarathi@gmail.com>
2022-12-06 15:07:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+var menubtn = document.getElementById("menubtn")
+            var sidenav = document.getElementById("sidenav")
+            var menu = document.getElementById("menu")
+    
+            sidenav.style.right == "-250px";
+    
+            menubtn.onclick = function(){
+                if(sidenav.style.right == "-250px"){
+                    sidenav.style.right = "0";
+                    menu.src="images/close.png";
+                }
+                else{
+                    sidenav.style.right = "-250px"
+                    menu.src="images/menu.png";
+                }
+            }
+
+            $('.clients-carousel').owlCarousel({
+                autoplay: true,
+                loop: true,
+                margin: 15,
+                dots: false,
+                slideTransition: 'linear',
+                autoplayTimeout: 4500,
+                autoplayHoverPause: true,
+                autoplaySpeed: 4500,
+                responsive: {
+                  0: {
+                    items: 2
+                  },
+                  500: {
+                    items: 3
+                  },
+                  600: {
+                    items: 4
+                  },
+                  800: {
+                    items: 4
+                  },
+                  1200: {
+                    items: 4
+                  }
+         
+                }
+              });
+              
+             /*login page*/
+             
